825a6b86f2f9c3284a898b162cf60aeabd364edb
SandeepReddyVeldhi7 <veldhisandeepreddy@gmail.com>
2025-10-23 18:35:16 +0530

    asdsa

---
+++ src/app/(admin)/admin/page.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const page = () => {
+  return (
+    <div className="flex justify-center items-center w-full h-[80vh]  p-6">
+      <div className=" rounded-2xl shadow-xl p-10 w-full h-auto max-w-4xl text-center flex flex-col justify-center items-center">
+        <div>
+          <h1 className="text-5xl font-extrabold text-gray-900 mb-4 ">Welcome to Admin Dashboard</h1>
+          <p className="text-xl text-gray-700 mb-8">Your gateway to seamless administration. Start managing with confidence today.</p>
+        </div>
+      
+      </div>
+    </div>
+  );
+};
+
+export default page;
