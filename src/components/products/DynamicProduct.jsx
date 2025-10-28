53900b4ec91f1dd74c66c219b81517b60e6b5230
jeevanjames2000 <jeevanjames2000@gmail.com>
2025-10-28 14:19:32 +0530

    rm banner

--- src/components/products/DynamicProduct.jsx
+++ src/components/products/DynamicProduct.jsx
@@ -1,13 +1,13 @@
 "use client";
-import Banner from "./Dynamic/Banner";
 import AboutSection from "./Dynamic/AboutSection";
 import Advantages from "./Dynamic/Advantages";
 import Faqs from "./Dynamic/Faqs";
+import PageBanner from "../PageBanner";
 
 export default function DynamicProduct({ product }) {
   return (
     <div className="bg-gradient-to-br from-white via-[#f8fbff]/50 to-[#e6f0ff]/50">
-      <Banner title={product.title} subtitle={product.subtitle} />
+      <PageBanner title={product.title} subtitle={product.subtitle} />
       <section className="max-w-6xl mx-auto py-12 lg:py-16 px-4 sm:px-6 lg:px-8">
         <AboutSection product={product} />
         <Advantages product={product} />
