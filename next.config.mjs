8c2e594287aa38e43150a6e1263565473964bd8b
jeevanjames2000 <jeevanjames2000@gmail.com>
2025-10-29 14:56:27 +0530

    sd

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["images.pexels.com", "localhost"],
+    domains: ["images.pexels.com", "localhost", "34.14.131.49"],
     remotePatterns: [
       {
         protocol: "http",
         hostname: "localhost",
         port: "3000",
+        pathname: "/uploads/**",
+      },
+      {
+        protocol: "http",
+        hostname: "34.14.131.49",
         pathname: "/uploads/**",
       },
       {
