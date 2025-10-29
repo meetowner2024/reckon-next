9b64c4c521e5d2d2757b46aff58cc09c70d51255
jeevanjames2000 <jeevanjames2000@gmail.com>
2025-10-29 11:19:12 +0530

    wtsapp

--- src/app/layout.js
+++ src/app/layout.js
@@ -18,27 +18,29 @@ const geistMono = Geist_Mono({
 });
 
 export default function RootLayout({ children }) {
-    const pathname = usePathname(); 
-  const isAdminPage = pathname.startsWith("/admin"); 
+  const pathname = usePathname();
+  const isAdminPage = pathname.startsWith("/admin");
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased   `}>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased   `}
+      >
         <Provider store={store}>
           <PersistGate loading={null} persistor={persistor}>
             {children}
 
-           {!isAdminPage && (
-            <a
-              href="https://wa.me/8886077754" 
-              target="_blank"
-              rel="noopener noreferrer"
-              aria-label="Chat on WhatsApp"
-              className="fixed bottom-6 right-6 flex items-center justify-center bg-green-500 text-white rounded-full w-14 h-14 shadow-lg hover:bg-green-600 transition-all duration-300 z-50 animate-bounce hover:scale-110 hover:shadow-green-400/50"
-            >
-              <FaWhatsapp size={30} className="drop-shadow-md" />
-              <span className="absolute w-full h-full rounded-full bg-green-400 opacity-20 animate-ping"></span>
-            </a>
-           )}
+            {!isAdminPage && (
+              <a
+                href="https://wa.me/8886077745?text=Hi%2C%20I%27d%20like%20to%20know%20more%20about%20your%20services."
+                target="_blank"
+                rel="noopener noreferrer"
+                aria-label="Chat on WhatsApp"
+                className="fixed bottom-6 right-6 flex items-center justify-center bg-green-500 text-white rounded-full w-14 h-14 shadow-lg hover:bg-green-600 transition-all duration-300 z-50 animate-bounce hover:scale-110 hover:shadow-green-400/50"
+              >
+                <FaWhatsapp size={30} className="drop-shadow-md" />
+                <span className="absolute w-full h-full rounded-full bg-green-400 opacity-20 animate-ping"></span>
+              </a>
+            )}
           </PersistGate>
         </Provider>
       </body>
